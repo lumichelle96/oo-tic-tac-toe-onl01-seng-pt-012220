74e9129212f5593d39eb3279f4de42e6fde4b941
Michelle Lu <lu.michelle96@icloud.com>
2020-02-08 22:57:44 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,4 +1,88 @@
-WIN_COMBINATIONS = [
-    [0,1,2],
-    []
-  ]
+class TicTacToe
+  
+    WIN_COMBINATIONS = 
+    [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [1,4,7],
+      [0,4,8],
+      [2,5,8],
+      [6,4,2]
+      
+    ]
+    
+    def initialize
+      @board = Array.new(9," ")
+    end
+    
+    def display_board
+      puts "#{@board[0]} | #{@board[1]} | #{@board[2]}"
+      puts "------------"
+      puts "#{@board[3]} | #{@board[4]} | #{@board[5]}"
+      puts "------------"
+      puts "#{@board[6]} | #{@board[7]} | #{@board[8]}"
+    end
+    
+    def input_to_index(user_input)
+      user_input.to_i - 1
+    end
+    
+    def move(index, current_player = "X")
+      @board[index] = current_player
+    end
+    
+    def position_taken?(index)
+      !(@board[index].nil? || @board[index] == " ")
+    end
+    
+    def valid_move?(index)
+      index.between?(0,8) && !position_taken?(index)
+    end
+    
+    def turn_count
+      turn = 0
+      @board.each do |index|
+        if index == "X" || index == "0"
+          turn += 1
+        end
+      end
+      return turn
+    end
+    
+    def current_player
+      num_turns = turn_count
+      if num_turns % 2 == 0
+        player = "X"
+      else
+        player = "O"
+      end
+      return player
+    end
+    
+    def turn 
+      puts "Please choose a number 1-9:"
+      user_input = gets.chomp!
+      index = input_to_index
+      if valid_move?(index)
+        player_token = current_player
+        move(index,player_token)
+        display_board
+      else
+        turn
+      end
+    end
+    
+    def won?
+      WIN_COMBINATIONS.each {|win_combo|
+      index_0 = win_combo[0]
+      index_1 = win_combo[1]
+      index_2 = win_combo[2]
+      
+      position_1 = @board[index_0]
+      position_2 = @board[index_1]
+      position_3 = @board[index_2]
+      
+      if 
+      
+      
