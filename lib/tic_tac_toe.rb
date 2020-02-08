78c4b271f13df12b5f636ed540b4763402bb3319
Michelle Lu <lu.michelle96@icloud.com>
2020-02-08 23:11:12 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -85,6 +85,13 @@ class TicTacToe
       
       if position_1 == "X" && position_2 == "X" && position_3 == "X"
         return win_combo
-      elsif 
+      elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+        return win_combo
+      end
+      }
+      return false
+    end
+    
+    def full?
       
       
