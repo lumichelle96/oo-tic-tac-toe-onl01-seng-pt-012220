59ce4cc8014955ebe5c6ec73abd7eba910e1f2d5
Michelle Lu <lu.michelle96@icloud.com>
2020-02-08 23:16:04 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -115,4 +115,20 @@ class TicTacToe
     def winner
       index = []
       index = won?
-      
+      if index == false
+      return nil
+      else
+        if @board[index[0]] == "X"
+          return "X"
+        else
+          return "O"
+        end
+      end
+    end
+    
+    def play
+      until over? == true
+      turn
+    end
+    
+    
